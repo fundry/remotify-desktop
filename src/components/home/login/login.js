4d338e4e4ae0be4a99b32bb81696f93307804f96
Nwani Victory <vickywane@gmail.com>
2019-06-17 14:50:52 +0100

    AApp layser

---
+++ src/components/home/login/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import Flex from 'styled-flex-component';
+
+const login = () => {
+	const Div = styled.div`
+		padding: 1em;
+	`;
+
+	return (
+		<Div>
+			<Flex justifyCenter>
+				<h1> ebkfl</h1> 
+			</Flex>
+		</Div>
+	);
+};
+export default login;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as Login  } from './home/login/login'
