2589686b87f9e4f060ec4b7e8dc9d4889cf033cc
Nwani Victory <vickywane@gmail.com>
2019-07-06 12:24:18 +0100

    Authentication

--- src/screens/main.js
+++ src/screens/main.js
@@ -5,13 +5,23 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import Routes from './routes';
 
+const electron = window.require('electron');
+const ipc = electron.ipcRenderer;
+
 class NavBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       data: undefined,
+      loggedIn: true,
     };
+  }
+
+  componentDidMount() {
+    if (this.state.loggedIn === false) {
+      ipc.send('authenticate-user');
+    }
   }
 
   render() {
@@ -24,10 +34,9 @@ class NavBar extends Component {
       <Div>
         <Flex>
           <div>
-              <p> Home </p>
-              <p> Settings </p>
+            <p> Home </p>
+            <p> Settings </p>
           </div>
-
         </Flex>
       </Div>
     );
