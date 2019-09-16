80962e1919c26588b00c140e1ade06a3d9762ed6
Nwani Victory <vickywane@gmail.com>
2019-09-16 01:04:35 +0100

    Fail tests

--- src/test/spectron.test.js
+++ src/test/spectron.test.js
@@ -1,32 +1,31 @@
 import { Application } from 'spectron';
 import path from 'path';
 import assert from 'assert';
+import 'regenerator-runtime/runtime';
 
 const baseDir = path.join(__dirname, '..');
 const electronbinary = path.join(baseDir, 'node_modules', '.bin', 'electron');
 
 // base test block
 describe('Should Launch App ', () => {
-  setTimeout(() => {}, 3000);
-
   const app = new Application({
     path: electronbinary,
     args: [baseDir],
   });
 
-  () => app.start();
+  // app.start().then(function() {
+  //   // Check if the window is visible
 
-  () => app.stop();
+  //   return app.browserWindow.isVisible();
+  // });
+
+  beforeEach(() => app.start());
+
+  // afterEach(() => app.stop());
 
   it('shows first window', async () => {
     await app.client.waitUntilWindowLoaded();
     const count = await app.client.getWindowCount();
     assert.equal(count, 1);
   });
-
-  //   const number = 5;
-
-  //   it('checks num', function() {
-  //     assert.equal(number, 1);
-  //   });
 });
