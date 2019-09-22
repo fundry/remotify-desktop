44c5980f6e7c6e4963e3b5c4d946f9841fd99e27
Nwani Victory <vickywane@gmail.com>
2019-09-22 20:52:44 +0100

    ..

--- src/test/spectron.test.js
+++ src/test/spectron.test.js
@@ -4,26 +4,25 @@ import electron from 'electron';
 import assert from 'assert';
 
 describe('Application launch', function() {
-  beforeEach(function() {
-    this.app = new Application({
-      path: electron,
+  const App = new Application({
+    path: electron,
 
-      args: [path.join(__dirname, '..', '..')],
-    });
-    return this.app.start();
+    args: [path.join(__dirname, '..', '..')],
   });
 
+  beforeAll(function() {
+    return App.start();
+  }, 50000);
+
   afterEach(function() {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
+    if (App && App.isRunning()) {
+      return App.stop();
     }
   });
 
   it('shows an initial window', function() {
-    return this.app.client.getWindowCount().then(function(count) {
+    return App.client.getWindowCount().then(function(count) {
       assert.equal(count, 1);
-      // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-      // assert.equal(count, 2)
     });
   });
 });
